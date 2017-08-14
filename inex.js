c4bfee980629286ee17727c7d19e319f7e697042
SryanZY <1650329050@qq.com>
2017-08-14 16:01:28 +0800

    new file

---
+++ inex.js
@@ -0,0 +1,15 @@
+class Type {
+	constructor(name, age) {
+		this.name = name;
+		this.age = age;
+	}
+	staticFunction () {
+		console.log('template');
+	}
+}
+
+class babel extends TRype {
+	constructor() {
+		super(name, age);
+	}
+}
